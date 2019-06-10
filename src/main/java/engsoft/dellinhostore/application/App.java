d42277430c88cba3331fdc55d5944f794f7d8b44
Andy Ruiz Garramones <argarramones@inf.ufrgs.br>
2019-06-09 22:10:21 -0300

    Changed name

---
+++ src/main/java/engsoft/dellinhostore/application/App.java
@@ -0,0 +1,17 @@
+package engsoft.dellinhostore.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "engsoft.dellinhostore.controller" })
+public class App {
+    public static void main( String[] args ) {
+    	System.out.println("Comecei\n\n\n\n");
+    	System.out.println("DATABASE_CUSTOM_URL : " + System.getenv("DATABASE_CUSTOM_URL"));
+    	System.out.println("DATABASE_USERNAME : " + System.getenv("DATABASE_USERNAME"));
+    	System.out.println("DATABASE_PASSWORD : " + System.getenv("DATABASE_PASSWORD"));
+    	SpringApplication.run(App.class, args);   
+    }  
+}
