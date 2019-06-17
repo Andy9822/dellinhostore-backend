335fd3dd318c483978c724e5d68f41c409e35395
Andy Ruiz Garramones <argarramones@inf.ufrgs.br>
2019-06-17 00:10:45 -0300

    Refactored to folow REST API pattern
    Mapped Post and Get http request methods

--- src/main/java/engsoft/dellinhostore/controller/NegotiationController.java
+++ src/main/java/engsoft/dellinhostore/controller/NegotiationController.java
@@ -3,6 +3,8 @@ package engsoft.dellinhostore.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import engsoft.dellinhostore.dao.AdvertDAO;
 import engsoft.dellinhostore.dao.ClientDAO;
 import engsoft.dellinhostore.dao.NegotiationDAO;
-import engsoft.dellinhostore.enums.TransactionStatus;
 import engsoft.dellinhostore.model.Advert;
 import engsoft.dellinhostore.model.Client;
-import engsoft.dellinhostore.model.Game;
 import engsoft.dellinhostore.model.Negotiation;
 import engsoft.dellinhostore.util.ReturnMessage;
 
@@ -23,27 +23,28 @@ import engsoft.dellinhostore.util.ReturnMessage;
 public class NegotiationController {
 
 	private NegotiationDAO nDao = new NegotiationDAO();
-	@RequestMapping("/insert")
-	public ReturnMessage insert(
-			@RequestParam(value = "advert_id") long advert_id,
-			@RequestParam(value = "offerer_id") long offerer_id,
-			@RequestParam(value = "description") String description) {
-		if (validInsertIds(advert_id,offerer_id)){
-			AdvertDAO aDao = new AdvertDAO();
-			ClientDAO cDao = new ClientDAO();
-			Advert advert = aDao.getById(advert_id);
-			Client offerer = cDao.getById(offerer_id);
-			Negotiation negotiation = new Negotiation(advert, offerer, description);
+
+	@PostMapping
+	public ReturnMessage insert(@RequestParam(value = "advert_id") long advert_id,
+			@RequestParam(value = "offerer_id") long offerer_id, @RequestParam(value = "offer") String offer) {
+		if (validInsertIds(advert_id, offerer_id, offer)) {
+			Negotiation negotiation = createNegotiation(advert_id, offerer_id, offer);
 			nDao.save(negotiation);
-			return new ReturnMessage(true,negotiation);
+			return new ReturnMessage(true, negotiation);
 		} else {
 			return new ReturnMessage(false, "Invalid Ids");
 		}
 	}
 
+	@GetMapping
+	public ReturnMessage getNegotiationList() {
+		List<Negotiation> negotiationList = nDao.getNegotiationList();
+		return new ReturnMessage(true, negotiationList);
+	}
+
+	//TODO test
 	@RequestMapping("/delete")
-	public ReturnMessage delete(
-			@RequestParam(value = "negotiation_id") long negotiation_id) {
+	public ReturnMessage delete(@RequestParam(value = "negotiation_id") long negotiation_id) {
 		Negotiation negotiation = nDao.getById(negotiation_id);
 		if (negotiation != null) {
 			nDao.delete(negotiation);
@@ -53,65 +54,42 @@ public class NegotiationController {
 		}
 	}
 
-	@RequestMapping("/accept")
-	public ReturnMessage accept(
-			@RequestParam(value = "negotiation_id") long negotiation_id) {
-		Negotiation negotiation = nDao.getById(negotiation_id);
-		//We have to check if the advert_id received exists
-		if (negotiation != null) {
-			//We only accept if the transaction was properly waiting for an answer
-			if (negotiation.getStatus() == TransactionStatus.WAITING_ANSWER) {
-				negotiation.acceptTransaction();
-				nDao.update(negotiation);
-				closeAdvertAndNegotiations(negotiation.getAdvert().getId());
-				createTrade(negotiation);
-				return new ReturnMessage(true, "Trade processed!");
-			} else {
-				return new ReturnMessage(false,"The transaction wasn't waiting for an answer so it couldn't accept the request");
-			}
-
-		} else {
-			return new ReturnMessage(false,"Invalid Id");
-		}
-	}
-
-	private void createTrade(Negotiation negotiation) {
-		TradeController trController = new TradeController();
-		Client advertiser = negotiation.getAdvertiser();
-		Client offerer = negotiation.getOfferer();
-		String offer = negotiation.getOffer();
-		Game game = negotiation.getAdvert().getAdvertisedGame();
-		trController.create(advertiser, offerer, game, offer);
-
-	}
-	
-	//We have to delete all negotiations for first because they have a reference to the advert and we need to guarantee integrity of the DB
-	private void closeAdvertAndNegotiations(long advert_id) {
-		//nDao.deleteNegotiationsByAdvertId(advert_id);
-		nDao.closeNegotiationsByAdvertId(advert_id);
-		AdvertController adController = new AdvertController();
-		adController.closeById(advert_id);
-
-	}
-	
-	@RequestMapping("/list")
-	public List<Negotiation> getNegotiationList() {
-		List <Negotiation> negotiationList = nDao.getNegotiationList();
-		return negotiationList;
-	}
-	
-
-	private boolean validInsertIds(long advert_id, long offerer_id) {
+	private Negotiation createNegotiation(long advert_id, long offerer_id, String offer) {
 		AdvertDAO aDao = new AdvertDAO();
 		ClientDAO cDao = new ClientDAO();
 		Advert advert = aDao.getById(advert_id);
 		Client offerer = cDao.getById(offerer_id);
-		if (advert != null && offerer != null) {
+		Negotiation negotiation = new Negotiation(advert, offerer, offer);
+		return negotiation;
+	}
+
+	private boolean validInsertIds(long advert_id, long offerer_id, String offer) {
+		AdvertDAO aDao = new AdvertDAO();
+		ClientDAO cDao = new ClientDAO();
+		Advert advert = aDao.getById(advert_id);
+		Client offerer = cDao.getById(offerer_id);
+		if (advert != null && offerer != null && !offer.trim().equals("")) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
+	/*
+	 * Methods to make easier jUnit tests
+	 */
+	public long insertManually(long advert_id, long offerer_id, String offer) {
+		Negotiation negotiation = createNegotiation(advert_id, offerer_id, offer);
+		nDao.save(negotiation);
+		return negotiation.getId();
+	}
+
+	static public void deleteTestedNegotiation(String offer) {
+		NegotiationDAO nDao = new NegotiationDAO();
+		Negotiation negotiation = nDao.getByDescription(offer);
+		if (negotiation != null) {
+			nDao.delete(negotiation);
+		}
+	}
 
 }
--- src/main/java/engsoft/dellinhostore/controller/TradeController.java
+++ src/main/java/engsoft/dellinhostore/controller/TradeController.java
@@ -2,17 +2,24 @@ package engsoft.dellinhostore.controller;
 
 import engsoft.dellinhostore.model.Client;
 import engsoft.dellinhostore.model.Game;
+import engsoft.dellinhostore.model.Negotiation;
 import engsoft.dellinhostore.model.Rating;
 import engsoft.dellinhostore.model.Trade;
+import engsoft.dellinhostore.util.ReturnMessage;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import engsoft.dellinhostore.dao.NegotiationDAO;
 import engsoft.dellinhostore.dao.TradeDAO;
+import engsoft.dellinhostore.enums.AdvertStatus;
+import engsoft.dellinhostore.enums.TransactionStatus;
 
 @CrossOrigin
 @RestController
@@ -21,19 +28,22 @@ public class TradeController {
 	
 	private TradeDAO tDao = new TradeDAO();
 	
-	protected Trade create(Client advertiser, Client offerer, Game game, String description) {
-		RatingController ratController = new RatingController();
-		Rating rating = new Rating();
-		rating = ratController.create();
-		Trade trade =  new Trade(advertiser,offerer,game,description,rating);
-		tDao.save(trade);
-		return trade;
+	@PostMapping
+	public ReturnMessage insert(@RequestParam(value = "negotiation_id") long negotiation_id) {
+		if (validInsertParams(negotiation_id)) {
+			Trade trade = createTrade(negotiation_id);	
+			tDao.save(trade);
+			return new ReturnMessage(true, trade);
+		}
+		else {
+			return new ReturnMessage(false, "Invalid negotiation");
+		}
 	}
-	
-	@RequestMapping("/list")
-	public List<Trade> getTradeList() {
+
+	@GetMapping
+	public ReturnMessage getTradeList() {
 		List <Trade> tradeList = tDao.getTradeList();
-		return tradeList;
+		return new ReturnMessage(true, tradeList);
 	}
 	
 	@RequestMapping("/listByAdvertiserId")
@@ -41,6 +51,46 @@ public class TradeController {
 			@RequestParam(value = "advert_Id") long advertiser_id) {
 		List <Trade> tradeList = tDao.getTradeList();
 		return tradeList;
+	}
+	
+	private Trade createTrade(long negotiation_id) {
+		NegotiationDAO nDao = new NegotiationDAO();
+		Negotiation negotiation  = nDao.getById(negotiation_id);
+		Client advertiser = negotiation.getAdvert().getAdvertiser();
+		Client offerer = negotiation.getOfferer();
+		Game advertisedGame = negotiation.getAdvert().getAdvertisedGame();
+		RatingController ratController = new RatingController();
+		Rating rating = ratController.create();
+		Trade trade =  new Trade(advertiser, offerer, advertisedGame, negotiation.getOffer(), rating);
+		return trade;
+	}
+	
+	private boolean validInsertParams(long negotiation_id) {
+		NegotiationDAO nDao = new NegotiationDAO();
+		Negotiation negotiation  = nDao.getById(negotiation_id);
+		if (negotiation != null && negotiation.getStatus() == TransactionStatus.WAITING_ANSWER && negotiation.getAdvert().getStatus() == AdvertStatus.OPEN) {
+			return true;
+		}
+		else {
+			return false;	
+		}
+	}
+	
+	/*
+	 * Methods to make jUnit tests easier
+	 */
+	public long insertManually(long negotiation_id) {
+		Trade trade = createTrade(negotiation_id);
+		tDao.save(trade);
+		return trade.getId();
+	}
+	
+	static public void deleteTestedTrade(String offer) {
+		TradeDAO tDao = new TradeDAO();
+		Trade trade = tDao.getByDescription(offer);
+		if (trade  != null) {
+			tDao.delete(trade);
+		}
 	}
 	
 }
